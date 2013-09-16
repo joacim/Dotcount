2324e7954f090a8ab3547dc2ef781dfb16af881c
Joacim Thomassen <joacim.thomassen@gmail.com>
2013-09-16 22:05:03 +0200

    Minor metadata update on copyright and e-mail.

--- src/dotcount.cpp
+++ src/dotcount.cpp
@@ -1,5 +1,5 @@
 /* Dotcount - This program counts black separate regions (dots) in an image.
- * Copyright (C) 2009-2011  Joacim Thomassen
+ * Copyright (C) 2009-2013  Joacim Thomassen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,8 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * The author can be contacted by e-mail at this address:
- * joacim@net.homelinux.org
  *
  * Initial date: 28-1-2009
  * Changed date: 30-7-2011
